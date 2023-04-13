d36c25bde57e6041f1721fc1df108e93857cd641
sadcof <sadsidecoff@gmail.com>
2023-04-13 18:04:41 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polyclinic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = 60;
+            int minutesInCabinet = 10;
+
+            Console.Write("Сочувствую! Вы оказались в поликлинике! Введите количество бабулек, что перед вами: ");
+            int grannysCount = Convert.ToInt32(Console.ReadLine());
+
+            int waitingHours = (grannysCount * minutesInCabinet) / hour;
+            int waitingMinutes = (grannysCount * minutesInCabinet) % hour;
+
+            Console.WriteLine($"Ваше время ожидания в очереди: {waitingHours} ч {waitingMinutes} мин");
+            Console.ReadKey();
+        }
+    }
+}
